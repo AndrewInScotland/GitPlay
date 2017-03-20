6e9a79c77ce87f072c2f7eebdecdcb0887249b1c
andrew.wallace <andrew.wallace@trov.com>
2017-03-20 09:31:09 -0400

    First commit

---
+++ GitPlay/GitPlayConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitPlayConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Play");
+
+            Console.WriteLine("Enter...");
+
+            Console.ReadLine();
+        }
+    }
+}
